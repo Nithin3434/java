ca5d9e7a55e3abca6994b7a4d115790f19a9aa9d
Nithin3434 <32122744+Nithin3434@users.noreply.github.com>
2017-10-11 19:12:43 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class input
+{
+public static void main(String args[])
+{
+int a,b,c,d;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+d=sc.nextInt();
+
+if(a==13)
+{
+System.out.println(c+d);
+}
+else if(a==13)
+{
+System.out.println(d+a);
+}
+else if(a==13)
+{
+System.out.println(a+b);
+}
+else if(a==13)
+{
+System.out.println(b+c);
+}
+else
+{
+System.out.println(a+b+c+d);
+}
+}
+}
